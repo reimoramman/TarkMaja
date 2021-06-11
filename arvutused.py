57546237f35ce25ccc122daf248f42e8efb941d8
reimoramman <reimoramman@online.ee>
2021-06-11 14:17:13 +0300

    viies päev

--- arvutused.py
+++ arvutused.py
@@ -1,8 +1,36 @@
-from datetime import datetime, timedate, date
+from datetime import datetime, timedelta, date
+import urllib.request, json
+import os
+import datetime
+import time
+import csv
+import re
+
 
 def timeOfWeek():
-  dayNr = datetime.datatime.today().weekday()
+  dayNr = datetime.datetime.today().weekday()
   if dayNr<5:
     return('Weekday')
   else:
     return('Weekend')
+
+#timeOfWeek()
+#funktsioon timeOfWeek t22tab
+
+def splitPrices():
+  cheapTimes=[]
+  expensiveTimes=[]
+  dayTotal=0
+  with open ('hinnad.csv', 'rt') as todaysPrices:
+    hinnad= csv.reader(todaysPrices)
+    next(hinnad)
+    for line in hinnad:
+      hinnad=("\t".join(line))
+      hinnad1=re.sub(r"[\n\t\s]*", "", hinnad)
+      print(hinnad+hinnad1)
+
+splitPrices()
+
+myString = "I want to Remove all white \t spaces, new lines \n and tabs \t"
+output   = re.sub(r"[\n\t\s]*", "", myString)
+print(myString+output)

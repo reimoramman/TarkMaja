e50b8c0fd7a20b62f30983263c0a398299705948
Admin <har0216@tlu.ee>
2021-06-16 22:06:44 +0300

    Õiged hinnad

--- 16.6/2/hinnad.py
+++ 16.6/2/hinnad.py
@@ -7,16 +7,14 @@ import urllib.error
 def GetPrices():
 
     url = 'https://dashboard.elering.ee/api/nps/price/csv'
-
-    today = datetime.datetime.now().astimezone('Europe/Tallinn')
+    
+    today = datetime.datetime.now().astimezone(pytz.timezone('Europe/Tallinn'))
     tomorrow = today + datetime.timedelta(1,0,0,0,0,0,0)
 
-    todaystr = today.strftime('20%y-%m-%d %X')
-    tomorrowstr = tomorrow.strftime('20%y-%m-%d %X')
+    todaystr = today.strftime('20%y-%m-%dT%X.999Z')
+    tomorrowstr = tomorrow.strftime('20%y-%m-%dT%X.999Z')
     todaystr = todaystr.replace(':', '%3A')
     tomorrowstr = tomorrowstr.replace(':', '%3A')
-    todaystr = todaystr.replace(' ', '%20')
-    tomorrowstr = tomorrowstr.replace(' ', '%20')
 
     file = url+'?start='+todaystr+'&end='+tomorrowstr+'&fields=ee'
     path = 'hinnad.csv'
